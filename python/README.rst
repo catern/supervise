0199e9f02c03a4dd1ca3af6ebbad6a4da15dde82
Spencer Baugh <sbaugh@catern.com>
2017-04-19 02:16:11 +0000

    properly setup a Python package

---
+++ python/README.rst
@@ -0,0 +1,4 @@
+supervise_api
+-------------
+
+This provides a safer subprocess API for Python based on the `supervise` utility.
---
+++ python/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='supervise_api',
+      version='0.1.2',
+      description='An API for running processes safely and securely',
+      long_description=("This package uses the supervise utility, a separately-available C binary,"
+                        " to provide a better process API for Linux."),
+      classifiers=[
+          "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+          "Operating System :: POSIX :: Linux",
+      ],
+      keywords='linux subprocess processes',
+      url='https://github.com/catern/supervise',
+      author='catern',
+      author_email='sbaugh@catern.com',
+      license='LGPLv3+',
+      packages=['supervise_api'])
