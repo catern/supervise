e491e734df5a0943cd35b0f3dffbff977d311d96
Spencer Baugh <sbaugh@catern.com>
2018-02-14 15:00:25 +0000

    supervise_api: up version to 0.4.0

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='supervise_api',
-      version='0.3.0',
+      version='0.4.0',
       description='An API for running processes safely and securely',
       long_description=("This package uses the supervise utility, a separately-available C binary,"
                         " to provide a better process API for Linux."),
